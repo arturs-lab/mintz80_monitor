f3801d568c982aa556324f5b249237fd1237a859
arturj <mac_it_man@yahoo.com>
2025-07-08 23:26:26 -0500

    add code to init my board add changes to make code assemble with my assembler add more functionality:   memmap_init   dmpio   beep   delay

--- Z80Monitor.asm
+++ Z80Monitor.asm
@@ -7,44 +7,46 @@
 ;  CREATE DATE :	05 May 15 / 2022-03-28
 ;***************************************************************************
 
-VERSMYR:    EQU     '1'
-VERSMIN:    EQU     '0'
+VERSMYR:    EQU     "1"
+VERSMIN:    EQU     "0"
 
-            INCLUDE CONSTANTS.asm ; copy or edit one of the 
+            INCLUDE "CONSTANTS.asm" ; copy or edit one of the 
                                   ; CONSTANTS-aaaa-pp.asm files to
                                   ; CONSTANTS.asm
-SCAN        EQU     005FEh
+SCAN:        EQU     005FEh
 
 ;ROM_BOTTOM:  EQU    0F000h		; Bottom address of ROM
-ROM_TOP:     EQU    ROM_BOTTOM + 00FFFh		; Top address of ROM
+ROM_TOP:     EQU    ROM_BOTTOM + 01FFFh		; Top address of ROM
 
 ;RAM_BOTTOM:  EQU    01800h		; Bottom address of RAM
-RAM_TOP:     EQU    RAM_BOTTOM + 1FFh		; Top address of RAM	
+;RAM_TOP:     EQU    RAM_BOTTOM + 1FFFh		; Top address of RAM	
+RAM_TOP:     EQU    $ffff
 
 ;UART_BASE:  EQU     0E0h        ; Base port address, DART uses 4 ports
 
+MONVARS	EQU	RAM_TOP - $1FF	; SP goes at the top of memory. Put monitor vars and buffers 511 bytes below it
 MPFMON:     EQU    0000h
-ASCDMPBUF:  EQU    RAM_BOTTOM + 0h      ;Buffer to construct ASCII part of memory dump
-ASCDMPEND:  EQU    RAM_BOTTOM + 10h     ;End of buffer, fill with EOS
-DMPADDR:    EQU    RAM_BOTTOM + 11h     ;Last dump address
-MVADDR:     EQU    RAM_BOTTOM + 12h     ; 6 bytes: start-address, end-address, dest-address or fill-value (23, 24, 25, 26, 27, 28)
-ERRFLAG:    EQU    RAM_BOTTOM + 18h     ; Location to store 
-MUTE:       EQU    RAM_BOTTOM + 19h     ; 0 - print received chars, 1 - do not print received chars
-ULSIZE:     EQU    RAM_BOTTOM + 1Ah     ; actual size of current/last hex-intel message
-IECHECKSUM: EQU    RAM_BOTTOM + 1Bh        ; hex-intel record checksum
-IECADDR:    EQU    RAM_BOTTOM + 1Ch        ; hex-intel record address (2 bytes)
-IERECTYPE:  EQU    RAM_BOTTOM + 1Eh        ; hex-intel record type
-DEBUG:      EQU    RAM_BOTTOM + 1Fh
-MTPHFLAG    EQU    RAM_BOTTOM + 1Fh     ; Phase counter: phase 1 doesn't check old value (being unknown)
-RX_READ_P:  EQU    RAM_BOTTOM + 20h     ; read pointer
-RX_WRITE_P: EQU    RAM_BOTTOM + 22h     ; write pointer
-CHKSUM_C:   EQU    RAM_BOTTOM + 24h     ; uses 3 bytes
-CF_SECCNT:  EQU    RAM_BOTTOM + 27h 
-CF_LBA0:    EQU    RAM_BOTTOM + 28h
-CF_LBA1:    EQU    RAM_BOTTOM + 29h
-CF_LBA2:    EQU    RAM_BOTTOM + 2Ah
-CF_LBA3:    EQU    RAM_BOTTOM + 2Bh
-UPLOADBUF:  EQU    RAM_BOTTOM + 2Ch     ; Buffer for hex-intel upload. Allows up to 32 bytes (20h) per line.
+ASCDMPBUF:  EQU    MONVARS + 0h      ;Buffer to construct ASCII part of memory dump
+ASCDMPEND:  EQU    MONVARS + 10h     ;End of buffer, fill with EOS
+DMPADDR:    EQU    MONVARS + 11h     ;Last dump address
+MVADDR:     EQU    MONVARS + 12h     ; 6 bytes: start-address, end-address, dest-address or fill-value (23, 24, 25, 26, 27, 28)
+ERRFLAG:    EQU    MONVARS + 18h     ; Location to store 
+MUTE:       EQU    MONVARS + 19h     ; 0 - print received chars, 1 - do not print received chars
+ULSIZE:     EQU    MONVARS + 1Ah     ; actual size of current/last hex-intel message
+IECHECKSUM: EQU    MONVARS + 1Bh        ; hex-intel record checksum
+IECADDR:    EQU    MONVARS + 1Ch        ; hex-intel record address (2 bytes)
+IERECTYPE:  EQU    MONVARS + 1Eh        ; hex-intel record type
+DEBUG:      EQU    MONVARS + 1Fh
+MTPHFLAG:	 EQU    MONVARS + 1Fh     ; Phase counter: phase 1 doesn't check old value (being unknown)
+RX_READ_P:  EQU    MONVARS + 20h     ; read pointer
+RX_WRITE_P: EQU    MONVARS + 22h     ; write pointer
+CHKSUM_C:   EQU    MONVARS + 24h     ; uses 3 bytes
+CF_SECCNT:  EQU    MONVARS + 27h 
+CF_LBA0:    EQU    MONVARS + 28h
+CF_LBA1:    EQU    MONVARS + 29h
+CF_LBA2:    EQU    MONVARS + 2Ah
+CF_LBA3:    EQU    MONVARS + 2Bh
+UPLOADBUF:  EQU    MONVARS + 2Ch     ; Buffer for hex-intel upload. Allows up to 32 bytes (20h) per line.
 ULBUFSIZE:  EQU    50h                  ; a 20h byte hex-intel record use 75 bytes...
 ULBEND:     EQU    UPLOADBUF + ULBUFSIZE
 MSGBUF:     EQU    UPLOADBUF
@@ -69,6 +71,7 @@ CR:         EQU    00Dh
 
 
             ORG ROM_BOTTOM
+
 ROUTINES:
 R_MAIN:     JP      MAIN            ; init DART and starts command loop
 R_U_INIT:   JP      UART_INIT       ; configures DARTchannel B 
@@ -85,55 +88,72 @@ R_PRT_STR:  JP      PRINT_STRING    ; sends a NULL terminated string
 ;Function: Entrance to user program
 ;***************************************************************************
 MAIN:
-			LD		SP,RAM_TOP			;Load the stack pointer for stack operations.
-			CALL	UART_INIT			;Initialize UART
-			CALL	PRINT_MON_HDR		;Print the monitor header info
-			LD		A, 00h
-			LD		(DMPADDR), A
-			LD		A, 0FFh				; FF00h and next should result in 0000h
-			LD		(DMPADDR+1), A
-			CALL	CLEAR_ERROR
-			CALL    MON_PROMPT_LOOP		;Monitor user prompt loop
-			HALT
+	di
+	ld a,$7b	; disable wdt
+	out ($f0),a
+	ld a,$b1
+	out ($f1),a
+	ld sp,RAM_TOP
+;	call memmap_init
+	ld a,$20
+	call delay		; looks like Z80 needs this delay to successfully write to IO ports
+	ld a,$01		; (SYSCLK MHz/2/(value+1))
+	out (turbo),a
+	call ayinit
+	ld bc,$0200	; bc = duration
+	ld a,$06		; a = pitch
+	call beep
+dio:	ld hl,hellostr
+	call PRINT_STRING
+	call dmpio
+	jr gomain
+
+hellostr:	db $0d,$0a,"Hellorld",$0d,$0a,"CPLD config:",0
+
+gomain:	LD SP,RAM_TOP		;Load the stack pointer for stack operations.
+	CALL UART_INIT		;Initialize UART
+	CALL PRINT_MON_HDR	;Print the monitor header info
+	LD A, 00h
+	LD (DMPADDR), A
+	LD A, 0FFh		; FF00h and next should result in 0000h
+	LD (DMPADDR+1), A
+	CALL CLEAR_ERROR
+	CALL MON_PRMPT_LOOP	;Monitor user prompt loop
+	HALT
 
 ;***************************************************************************
 ;CLEAR_SCREEN
 ;Function: Clears terminal screen
 ;***************************************************************************
-MON_CLS: DEFB 0Ch, EOS  				;Escape sequence for CLS. (aka form feed) 
-		
 CLEAR_SCREEN:
-			LD 		HL,MON_CLS
-			CALL    PRINT_STRING
-			RET
+	LD HL,MON_CLS
+	CALL PRINT_STRING
+	RET
 			
 ;***************************************************************************
 ;RESET_COMMAND
 ;Function: Software Reset to $0000
 ;***************************************************************************
 RESET_COMMAND:
-			JP		MPFMON				;Jumps to 0000 (MPF-1 monitor re-entry)	
+	JP MPFMON		;Jumps to 0000 (MPF-1 monitor re-entry)	
 			
 ;***************************************************************************
 ;PRINT_MON_HDR
 ;Function: Print out program header info
 ;***************************************************************************
-MNMSG1:     DEFB    0DH, 0Ah, 'ZMC80 Computer', 09h, 09h, 09h, '2015 MCook', EOS
-MNMSG2:     DEFB    0DH, 0Ah, ' adaptation to MPF-1 / Z80 DART', 09h, '2022 F.J.Kraan', 0Dh, 0Ah, EOS
-MNMSG3A:    DEFB    'Monitor v', VERSMYR, '.', VERSMIN, ', ROM: ', EOS
-MNMSG3B:    DEFB    'h, RAM: ', EOS
-MNMSG3C:    DEFB    'h, DART: ', EOS
-MNMSG3D:    DEFB    'h', 0Dh, 0AH, 0Dh, 0AH, EOS
-MONHLP:     DEFB    09h,' Input ? for command list', 0Dh, 0AH, EOS
-MONERR:     DEFB    0Dh, 0AH, 'Error in params: ', EOS
+MNMSG1:     DEFB    0DH, 0Ah, "AL80 Computer", 09h, 09h, 09h, "2015 MCook"
+MNMSG2:     DEFB    0DH, 0Ah, " adaptation to MPF-1 / Z80 DART", 09h, "2022 F.J.Kraan", 0Dh, 0Ah
+            DEFB    "Adaptation to AL80 2025 Artur's Lab", 0Dh, 0Ah
+MNMSG3A:    DEFB    "Monitor v", VERSMYR, ".", VERSMIN, ", ROM: ", EOS
+MNMSG3B:    DEFB    "h, RAM: ", EOS
+MNMSG3C:    DEFB    "h, DART: ", EOS
+MNMSG3D:    DEFB    "h", 0Dh, 0AH, 0Dh, 0AH
+MONHLP:     DEFB    09h," Input ? for command list", 0Dh, 0AH, EOS
+MONERR:     DEFB    0Dh, 0AH, "Error in params: ", EOS
 
 PRINT_MON_HDR:
         CALL    CLEAR_SCREEN        ;Clear the terminal screen
         LD      HL, MNMSG1          ;Print some messages
-        CALL    PRINT_STRING
-        LD      HL, MNMSG2          ;Print some extra message
-        CALL    PRINT_STRING
-        LD      HL, MNMSG3A         ; 1st part, version & ROM
         CALL    PRINT_STRING
         LD      HL, ROM_BOTTOM
         CALL    PRINTHWORD
@@ -147,15 +167,13 @@ PRINT_MON_HDR:
         CALL    PRINTHBYTE
         LD      HL, MNMSG3D         ; 4th part, line ending
         CALL    PRINT_STRING
-        LD      HL, MONHLP
-        CALL    PRINT_STRING
         RET
 
 ;***************************************************************************
 ;MON_PROMPT
 ;Function: Prompt user for input
 ;***************************************************************************			
-MON_PROMPT: DEFB '>', EOS
+MON_PROMPT: DEFB ">", EOS
 
 MON_PRMPT_LOOP:
         LD      A, 00h
@@ -167,7 +185,7 @@ MON_PRMPT_LOOP:
         CP      CR
         JR      Z, _MPL_CR
         CALL    PRINT_NEW_LINE  ; Print a new line
-_MPL_CR
+_MPL_CR:
         CALL    MON_COMMAND     ; Respond to user input
         CALL    PRINT_NEW_LINE  ; Print a new line
         JR      MON_PRMPT_LOOP
@@ -178,41 +196,43 @@ _MPL_CR
 ;***************************************************************************
 MON_COMMAND:    ; Inserted ERROR_CHK for all commands requiring input
         CALL    CLEAR_ERROR
-        CP      '?'
+        CP      "?"
         CALL    Z,HELP_COMMAND
-        CP      'D'
+        CP      "D"
         CALL    Z,MDCMD
-        CP      'C'
+        CP      "C"
         CALL    Z,CLEAR_SCREEN
-        CP      'O'
+        CP      "N"
+        CALL    Z,RICOMMAND
+        CP      "O"
         CALL    Z,PW_COMMAND
-        CP      'P'
+        CP      "P"
         CALL    Z,PSCOMMAND
-        CP      'Q'
+        CP      "Q"
         CALL    Z,UTERMTST
-        CP      'R'
+        CP      "R"
         CALL    Z,RESET_COMMAND
-        CP      'M'
+        CP      "M"
         CALL    Z,MOVE_COMMAND
-        CP      'F'
+        CP      "F"
         CALL    Z,FILL_COMMAND
-        CP      'G'
+        CP      "G"
         CALL    Z,GO_COMMAND
-        CP      'K'
+        CP      "K"
         CALL    Z,CL_COMMAND
-        CP      '+'
+        CP      "+"
         CALL    Z,NEXTP_COMMAND
-        CP      '-'
+        CP      "-"
         CALL    Z,PREVP_COMMAND
-        CP      'E'
+        CP      "E"
         CALL    Z,EDIT_COMMAND
-        CP      ':'
+        CP      ":"
         CALL    Z,HEXI_COMMAND
-        CP      'S'
+        CP      "S"
         CALL    Z,CCKSM_COMMAND
-        CP      'T'
+        CP      "T"
         CALL    Z, TRAM_COMMAND
-        CP      'Z'
+        CP      "Z"
         CALL    Z,REGDUMP_COMMAND
         CALL    ERROR_CHK
         RET
@@ -227,14 +247,14 @@ UTERMTST:
         CP      010h    ; A - 010h
         JR      C, _UTHEX
         CALL    PRINTHBYTE
-        LD      A, ' '
+        LD      A, " "
         CALL    PRINT_CHAR
         JR      UTERMTST
         
-_UTHEX
+_UTHEX:
         CALL    NIB2CHAR
         CALL    PRINT_CHAR
-        LD      A, ' '
+        LD      A, " "
         CALL    PRINT_CHAR
         JR      UTERMTST
         
@@ -262,9 +282,82 @@ CLEAR_ERROR:
         POP     AF
         RET
         
-        INCLUDE	DARTDriver.asm
-        INCLUDE	MONCommands.asm
-        INCLUDE	CONIO.asm
+        INCLUDE	"UARTDriver.asm"
+;        INCLUDE	"DARTDriver.asm"
+        INCLUDE	"MONCommands.asm"
+        INCLUDE	"CONIO.asm"
 ;        INCLUDE CFDriver.asm
 
-        END
+MON_CLS: DEFB 0Ch, EOS  				;Escape sequence for CLS. (aka form feed) 
+		
+
+;        END
+
+; triangular progression delay
+; at CPU CLK = 1.333MHz:
+; $1a - 9.76ms
+; $1b - 10.48ms
+; $59 - 100ms
+; at CPU CLK = 4MHz:
+; $01 - 52us -> 19.23kHz
+; $04 - 172us -> 5.814kHz
+; $2f - 9.84ms -> 102Hz
+; $30 - 10.24ms -> 97.656Hz
+; $6d - 49.8ms -> 20Hz
+; $78 - 60ms -> 16.666Hz
+; $9b - 99.6ms -> 10.04Hz
+; $9c - 100.8ms
+
+del00:	ld a,$00		; delay loop
+delay:	push af			; count delay
+del1:	dec a
+	jr nz,del1		; repeat till 0
+	pop af
+	dec a
+	jr nz,delay
+	ret
+
+; bc = duration, a = pitch
+beep:	push af
+bep1:	out (beepr),a
+	pop af
+	push af
+	call delay
+	dec bc
+	ld a,b
+	or c
+	jr nz,bep1
+	pop af
+	ret
+
+dmpio:	ld c,$d0
+		ld b,$0
+		CALL	PRINT_NEW_LINE
+dmpio1:	IN      A, (C)
+		CALL	PRINTHBYTE
+		inc c
+		ld a,c
+		cp $e0
+		jr nz,dmpio1
+		CALL	PRINT_NEW_LINE
+		ret
+
+memmap_init:	in a,(beepr)	; unlock memmap
+	ld a,0		; init mem map
+	out ($d8),a
+	ld a,1
+	out ($d9),a
+	out ($da),a
+	out ($db),a
+	out ($dc),a
+	out ($dd),a
+	out ($de),a
+	out ($df),a
+	out (beepr),a	; lock memmap
+	ret
+
+endprog	equ $
+
+	output_bin "z80monitor.bin",0,endprog    ; 
+	output_intel "z80monitor.hex",0,endprog    ;
+	output_list "z80monitor.lst"
