e542ea957c300a9854b10b14f24c5fe919be9104
arturj <mac_it_man@yahoo.com>
2025-07-08 23:03:41 -0500

    remove unused original files

--- CONSTANTS-2000-60.asm
+++
@@ -1,7 +0,0 @@
-; Constants, extracted to make the versioned file hardware agnostic
-
-ROM_BOTTOM: EQU    02000h		; Bottom address of ROM
-
-RAM_BOTTOM:  EQU    01800h		; Bottom address of RAM
-
-UART_BASE:  EQU     060h        ; Base port address, DART uses 4 ports
--- asm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-FILE=$1
-NOEXT=${FILE%%.*}; echo "$NOEXT"
-~/kryten/Programming/c/z80pack/z80asm/z80asm -v -fh -o$NOEXT -l $FILE
