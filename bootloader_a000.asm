7ca16ca9377e0a63090129fa1f2db1e336972017
Arturs Lab <mac_it_man@yahoo.com>
2025-10-27 18:18:22 -0500

    Switch to using CF buffer for check before copying to destination. This way if user chooses to not run the code, previous version will remain intact. Add confirmation before running even if checksum matches. Add label to this code which can be printed before running it.

--- bootloader_a000.asm
+++ bootloader_a000.asm
@@ -17,21 +17,27 @@ zoWarnFlow = true
 	cp a,$ff			; if (CFSECT_BUF + $0200) = $ff then no code to run
 	ret z		; otherwise presume this may be a valid code that can be jumped to
 
-	ld de,(boot_dest)		; if found code, copy it to $a000-$bcff
-	ld bc,$2000
-	ldir
+	ld hl,CFSECT_BUF_V+$200+$1fe0	; calculate location of label
+	ld a,(hl)
+	cp a," "			; if (boot_dest + $1fe0) < " " then no label to print
+	jr c,chksum_calc
+	cp a,$7f			; if (boot_dest + $1fe0) > "" then no label to print
+	jr nc,chksum_calc
+	call jCON_PRT_STR_SP	; otherwise print label
+zoWarnFlow = false
+	db "Found label: ",0
+zoWarnFlow = true
+	call jCON_PRT_STR
 
-	ld hl,(boot_dest)	; calculate and print checksum
+chksum_calc:	ld hl,CFSECT_BUF_V+$200	; calculate and print checksum
 	ld (MVADDR+0),hl
-	ld bc,$1d00-1
+	ld bc,$1fdf
 	add hl,bc
 	ld (MVADDR+2),hl
 	call jCCKSM_DO
 
 	ld de,hl			; preserve checksum
-	ld hl,(boot_dest)	; calculate location of checksum in loaded data
-	ld bc,$2000-3
-	add hl,bc
+	ld hl,CFSECT_BUF_V+$200+$1ffd	; calculate location of checksum in loaded data
 
 	cp a,(hl)			; check MSB of checksum
 	jr nz,cksm_invalid
@@ -43,6 +49,14 @@ zoWarnFlow = true
 	ld a,e
 	cp a,(hl)			; check MSB of checksum
 	jr nz,cksm_invalid
+
+	call jCON_PRT_STR_SP	; Checksum good, confirm running
+zoWarnFlow = false
+	db "Execute? (y/n) ",0
+zoWarnFlow = true
+	call jCON_GET_CHAR
+	cp a,"Y"
+	ret nz		; anything but "y"
 	jr boot_go
 
 cksm_invalid:	call jCON_PRT_STR_SP	; announce invalid checksum
@@ -53,7 +67,12 @@ zoWarnFlow = true
 	cp a,"Y"
 	ret nz
 
-boot_go:	call jCON_PRT_STR_SP	; announce jumping to loaded code
+boot_go:	ld hl,CFSECT_BUF_V+$200
+	ld de,(boot_dest)		; if found code, copy it to $a000-$bcff
+	ld bc,$2000
+	ldir
+
+	call jCON_PRT_STR_SP	; announce jumping to loaded code
 zoWarnFlow = false
 	db $0d,$0a,"Jumping to ",0
 zoWarnFlow = true
@@ -63,6 +82,10 @@ zoWarnFlow = true
 
 	jp (hl)			; and jump to beginning of it
 
+	org $c1a0
+code_label:	db "Monitor Loader A000",0		; label of this program for monitor loader to display
+
+	org $c1bc
 boot_dest	dw $a000		; destination address of boot code
 
 endprog	equ $
