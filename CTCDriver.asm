e39bb64e4f0db08b8d10e91bd4aa7b9ada9e62f3
Arturs Lab <mac_it_man@yahoo.com>
2025-11-13 19:39:25 -0600

    initialize interrupt ISRs, vectors, add ISR routines

--- CTCDriver.asm
+++ CTCDriver.asm
@@ -43,7 +43,23 @@ CTC3_INIT:	ld a,(CTC_CH3_CNF)      ; interrupt off, counter mode, prescaler=256 
         ret
 
 
-CTC_TC_INIT: ld hl,CTC_DEFAULTS
+CTC_TC_INIT: 	ld a,CTCV			; load CTC interrupt vector
+		out (CTC_CH0),a		; set CTC T0 to that vector
+		add a,2
+		out (CTC_CH1),a		; set CTC T1 to that vector
+		add a,2
+		out (CTC_CH2),a		; set CTC T2 to that vector
+		add a,2
+		out (CTC_CH3),a		; set CTC T3 to that vector
+
+		ld hl,SYSTMR0
+		ld b,10
+		xor a
+CTC_TC_INIT1:	ld (hl),a
+		inc hl
+		djnz CTC_TC_INIT1
+
+		ld hl,CTC_DEFAULTS
 		ld de,CTC_CH0_CNF
 		ld bc,8
 		ldir
@@ -52,3 +68,69 @@ CTC_TC_INIT: ld hl,CTC_DEFAULTS
 ; these are configured in CONSTANTS.asm
 CTC_DEFAULTS:	db CTC_CH0_CNFV,CTC_CH1_CNFV,CTC_CH2_CNFV,CTC_CH3_CNFV
 			db CTC_CH0_TV,CTC_CH1_TV,CTC_CH2_TV,CTC_CH3_TV
+
+; T0 ISR - increment system timer by 1 on every interrupt
+; 118 cycles when only lower counter incremented
+; 151 cycles when overflow into upper counter = 16.3845486us
+
+CTC_T0_ISR:	push hl	; 11c
+		push af		; 11c
+		ld hl,(SYSTMR0)	; 16c
+		inc hl		; 6c
+		ld (SYSTMR0),hl	; 16c
+		ld a,h		; 4c
+		or l			; 4c
+		jr nz,CTC_T0_X	; 12/7c
+		ld hl,(SYSTMR2)	; 16c
+		inc hl		; 6c
+		ld (SYSTMR2),hl	; 16c
+CTC_T0_X:	pop af		; 10c
+		pop hl		; 10c
+		ei			; 4c
+		reti			; 14c
+
+; T1 ISR - increment system timer by 1 on every interrupt
+; same as T0 but interrupt timing my differ
+
+CTC_T1_ISR:
+up_isr:	push af
+		ld a,(SYSTMR4)	; 5 milliseconds
+		inc a
+		cp 200
+		jr nz,up_isr_1
+		xor a
+up_isr_1:	ld (SYSTMR4),a
+		jr nz,up_isr_x
+
+		ld a,(SYSTMR5)	; seconds
+		inc a
+		cp 60
+		jr nz,up_isr_2
+		xor a
+up_isr_2:	ld (SYSTMR5),a
+		jr nz,up_isr_x
+
+		ld a,(SYSTMR6)	; minutes
+		inc a
+		cp 60
+		jr nz,up_isr_3
+		xor a
+up_isr_3:	ld (SYSTMR6),a
+		jr nz,up_isr_x
+
+		ld a,(SYSTMR7)	; hours
+		inc a
+		cp 60
+		jr nz,up_isr_4
+		xor a
+up_isr_4:	ld (SYSTMR7),a
+		jr nz,up_isr_x
+
+		ld hl,(SYSTMR8)	; days
+		inc hl
+		ld (SYSTMR8),hl
+
+up_isr_x:	pop af
+		ei
+		reti
+
