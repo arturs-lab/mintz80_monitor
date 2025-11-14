b697b527e28badc49c441748a0fc03f4c44c600e
Arturs Lab <mac_it_man@yahoo.com>
2025-11-13 20:21:56 -0600

    bugfix push/pop HL, move ISR vector init to individual channel init

--- CTCDriver.asm
+++ CTCDriver.asm
@@ -9,50 +9,49 @@ CTC_INIT_ALL: push af
 
 ;CH0 divides CPU CLK by (256*CTC_CH0_TC) providing a clock signal at TO0. TO0 is connected to TRG1.
 ; T01 outputs f= CPU_CLK/(256*CTC_CH0_TC) => 9.216MHz / ( 256 * 180 ) => 200Hz
-CTC0_INIT:	ld a,(CTC_CH0_CNF)      ; interrupt off; timer mode; prescaler=256; don't care ext; automatic trigger;
-                            ; time constant follows; cont. operation; command word
-        out (CTC_CH0),a
-        ld a,(CTC_CH0_TC)           ; time constant
-        out (CTC_CH0),a
+CTC0_INIT: ld a,CTCV		; load CTC interrupt vector
+	out (CTC_CH0),a		; set CTC T0 to that vector
+	ld a,(CTC_CH0_CNF)	; interrupt off; timer mode; prescaler=256; don't care ext; automatic trigger;
+					; time constant follows; cont. operation; command word
+	out (CTC_CH0),a
+	ld a,(CTC_CH0_TC)	; time constant
+	out (CTC_CH0),a
 
 ;init CH1
 ;CH1 disabled
-CTC1_INIT:	ld a,(CTC_CH1_CNF)      ; interrupt off, timer mode, prescaler=16, don't care ext. TRG edge,
-                            ; start timer on loading constant, time constant follows, software reset, command word
-        out (CTC_CH1),a         ; CH1 is halted
-        ld A,(CTC_CH1_TC)           ; time constant 56d
-        out (CTC_CH1),a         ; loaded into channel 1
+CTC1_INIT: ld a,CTCV+2	; load CTC interrupt vector
+	out (CTC_CH1),a		; set CTC T0 to that vector
+	ld a,(CTC_CH1_CNF)	; interrupt off, timer mode, prescaler=16, don't care ext. TRG edge,
+					; start timer on loading constant, time constant follows, software reset, command word
+	out (CTC_CH1),a		; CH1 is halted
+	ld A,(CTC_CH1_TC)	; time constant 56d
+	out (CTC_CH1),a		; loaded into channel 1
 
 ;init CH2
 ;CH2 divides CLK/TRG2 clock providing a clock signal at TO2.
 ; T02 outputs f= CLK/TRG / 0x34 => 4MHz / 52 => 38461
-CTC2_INIT:	ld a,(CTC_CH2_CNF)      ; interrupt off, counter mode, prescaler=256 (doesn't matter), ext. start,
-                            ; start upon loading time constant, time constant follows,sw reset, command word
-        out (CTC_CH2),a
-        ld A,(CTC_CH2_TC)           ; time constant 56d
-        out (CTC_CH2),a         ; loaded into channel 2
+CTC2_INIT: ld a,CTCV+4	; load CTC interrupt vector
+	out (CTC_CH2),a		; set CTC T0 to that vector
+	ld a,(CTC_CH2_CNF)	; interrupt off, counter mode, prescaler=256 (doesn't matter), ext. start,
+					; start upon loading time constant, time constant follows,sw reset, command word
+	out (CTC_CH2),a
+	ld A,(CTC_CH2_TC)	; time constant 56d
+	out (CTC_CH2),a		; loaded into channel 2
 
 ;init CH3
 ;CH3 divides CLK/TRG3 clock providing a clock signal at TO3.
 ; T03 outputs f= CLK/TRG / 4  => 4MHz / 4 => 1MHz
-CTC3_INIT:	ld a,(CTC_CH3_CNF)      ; interrupt off, counter mode, prescaler=256 (doesn't matter), ext. start,
-                            ; start upon loading time constant, time constant follows,sw reset, command word
-        out (CTC_CH3),a
-        ld A,(CTC_CH3_TC)           ; time constant 56d
-        out (CTC_CH3),a         ; loaded into channel 3
-        ret
+CTC3_INIT: ld a,CTCV+6	; load CTC interrupt vector
+	out (CTC_CH3),a		; set CTC T0 to that vector
+	ld a,(CTC_CH3_CNF)	; interrupt off, counter mode, prescaler=256 (doesn't matter), ext. start,
+					; start upon loading time constant, time constant follows,sw reset, command word
+	out (CTC_CH3),a
+	ld A,(CTC_CH3_TC)	; time constant 56d
+	out (CTC_CH3),a		; loaded into channel 3
+	ret
 
 
-CTC_TC_INIT: 	ld a,CTCV			; load CTC interrupt vector
-		out (CTC_CH0),a		; set CTC T0 to that vector
-		add a,2
-		out (CTC_CH1),a		; set CTC T1 to that vector
-		add a,2
-		out (CTC_CH2),a		; set CTC T2 to that vector
-		add a,2
-		out (CTC_CH3),a		; set CTC T3 to that vector
-
-		ld hl,SYSTMR0
+CTC_TC_INIT: ld hl,SYSTMR0
 		ld b,10
 		xor a
 CTC_TC_INIT1:	ld (hl),a
@@ -126,9 +125,11 @@ up_isr_3:	ld (SYSTMR6),a
 up_isr_4:	ld (SYSTMR7),a
 		jr nz,up_isr_x
 
+		push hl
 		ld hl,(SYSTMR8)	; days
 		inc hl
 		ld (SYSTMR8),hl
+		pop hl
 
 up_isr_x:	pop af
 		ei
