654564f3e5a3094a69218d03c6b60d5982ccbe8b
Arturs Lab <mac_it_man@yahoo.com>
2025-11-24 10:43:49 -0600

    load and run basic from EEPROM

---
+++ run_basic.asm
@@ -0,0 +1,28 @@
+
+
+		org $8000
+
+start:	ld a,03		; switch first 2 banks to RAM
+		out ($d8),a
+		out ($d9),a
+		ld a,02		; switch next two banks to EEPROM2,3
+		out ($da),a
+		out ($db),a
+		ld hl,$4000
+		ld de,$0000
+		ld bc,$4000
+		ldir
+		ld a,01		; switch bank 2,3 to RAM
+		out ($da),a
+		out ($db),a
+hot:		ld a,03		; switch first 2 banks to RAM. We can enter here if basic was already loaded
+		out ($d8),a
+		out ($d9),a
+		jp $0000
+
+endprog	equ $
+
+;	output_bin "run_basic.bin",start,endprog-start
+	output_intel "run_basic.hex",start,endprog-start
+	output_list "run_basic.lst"
+
