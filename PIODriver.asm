ac7919405223fad66d61f33d85f44f9657d7935d
Arturs Lab <mac_it_man@yahoo.com>
2025-12-24 19:04:27 -0600

    rewrite PIO init

--- PIODriver.asm
+++ PIODriver.asm
@@ -1,22 +1,35 @@
 ;initialize both PIO ports
-PIO_INIT:	ld a,PIO_CH0_CNFV      ; PIO channel A default config mode 3 (bit control)
-	ld (PIO_CH0_CNF),a
-	ld a,PIO_CH1_CNFV      ; PIO channel B default config mode 3 (bit control)
-	ld (PIO_CH1_CNF),a
-	call PIOA_INIT	; first initialize PIO A, then fall through to PIO B
+PIO_INIT:	push hl
+		push de
+		push bc
+		ld hl,PIO_DEFLT
+		ld de,PIOA_CNF
+		ld bc,PIO_DEFLT_END-PIO_DEFLT
 
-PIOB_INIT:	ld a,(PIO_CH1_CNF)
+		ldir
+
+		pop bc
+		pop de
+		pop hl
+
+		call PIOA_INIT	; first initialize PIO A, then fall through to PIO B
+
+PIOB_INIT:	ld a,(PIOB_CNF)
 		out (PIO_CB),a
-		ld a,$ff      ; set pins of port A to INPUT
+		ld a,$ff      ; set pins of port B to INPUT
 		out (PIO_CB),a
 		ret
 
-PIOA_INIT:	ld a,(PIO_CH0_CNF)
-        out (PIO_CA),a
-        ld a,$FF      ; set pins of port A to INPUT
-        out (PIO_CA),a
+PIOA_INIT:	ld a,(PIOA_CNF)
+		out (PIO_CA),a
+		ld a,$FF      ; set pins of port A to INPUT
+		out (PIO_CA),a
 
-        ret
+		ret
+
+PIO_DEFLT:	db PIOA_CNFV,PIOA_INT_CTRV,PIOA_INT_ENV,PIOB_CNFV,PIOB_INT_CTRV,PIOB_INT_ENV
+
+PIO_DEFLT_END:	EQU $
 
 PIOA_OUT:	out (PIO_DA),A
 	ret
